0933da6fc183b211c296d12904eecd276a3767e0
Alessandra Soni <a.vasundrasoni@hotmail.com>
2024-12-07 02:58:38 -0500

    Revert "Order Summary Displays - but needs correct user address"
    This reverts commit 1eeb505f5bedb3d21ce658e090f692581d4d7269.

--- onlinestore/app/checkout/page.js
+++ onlinestore/app/checkout/page.js
@@ -15,29 +15,12 @@ export default function CheckoutPage() {
   const router = useRouter();
   const [userData, setUserData] = useState(null);
 
-  /* Redirect to cart if cart is empty
+  // Redirect to cart if cart is empty
   useEffect(() => {
     if (cartItems.length === 0) {
       router.push('/cart');
     }
   }, [cartItems]);
-  */
-    // Fetch latest order and redirect to Order Summary page
-    useEffect(() => {
-      const fetchLatestOrderAndRedirect = async () => {
-        try {
-          const response = await fetch('/api/order/[orderId]/route');
-          if (response.ok) {
-            const order = await response.json();
-            router.push(`/OrderSummaryClient/${order.id}`);
-          } else {
-            console.error('Failed to fetch the latest order.');
-          }
-        } catch (error) {
-          console.error('Error fetching latest order:', error);
-        }
-      };
-    }, [isAuthenticated, router]); // Verifying authentication
 
   // Fetch user data if authenticated
   useEffect(() => {
